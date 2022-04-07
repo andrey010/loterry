86a16506b12a791ed303331abddbc7b9078b7288
andrey010 <andr780008@gmail.com>
2022-04-07 13:21:51 +0300

    created contract

---
+++ test/Lottery.test.js
@@ -0,0 +1,61 @@
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+
+
+const { interface, bytecode } = require('../compile')
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+  accounts = await web3.eth.getAccounts()
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: bytecode})
+    .send({from: accounts[0], gas: '1000000'})
+})
+
+describe('Loterry contract', () => {
+  it('deployes a contract', () => {
+    assert.ok(lottery.options.address)
+  })
+
+  it('allow multiple accounts to enter', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('0.02', 'ether')
+    })
+
+    await lottery.methods.enter().send({
+      from: accounts[1],
+      value: web3.utils.toWei('0.02', 'ether')
+    })
+
+    await lottery.methods.enter().send({
+      from: accounts[2],
+      value: web3.utils.toWei('0.02', 'ether')
+    })
+
+    const players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    })
+
+    assert.equal(accounts[0], players[0])
+    assert.equal(accounts[1], players[1])
+    assert.equal(accounts[2], players[2])
+    assert.equal(players.length, 3)
+  })
+
+  it('min eth value require to enter', async () => {
+    try {
+      await lottery.methods.enter().send({
+        from: accounts[0],
+        value: 0
+      })
+    } catch(e) {
+      assert.ok(e)
+    }
+  })
+
+})
